f708b966cde73dbe7e96c2a09d1b68e49897b923
Harish Chopade <harishchopade2004@gmail.com>
2026-02-21 16:13:02 +0530

    Started

---
+++ src/main/java/com/application/hirehub/HirehubApplication.java
@@ -0,0 +1,14 @@
+package com.application.hirehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirehubApplication.class, args);
+		System.out.println(" \n Hire Hub Job Application Started ...");
+	}
+
+}
