63942e489571dd382ba95234bf960ba433a24905
tejasdhade17 <tejasdhande1703@gmail.com>
2026-02-21 19:09:24 +0530

    Configured modelmapper

--- src/main/java/com/application/hirehub/HirehubApplication.java
+++ src/main/java/com/application/hirehub/HirehubApplication.java
@@ -2,6 +2,11 @@ package com.application.hirehub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.Conditions;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class HirehubApplication {
@@ -9,6 +14,16 @@ public class HirehubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HirehubApplication.class, args);
 		System.out.println(" \n Hire Hub Job Application Started ...");
+	}
+
+	@Bean
+	ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration() //get default config
+		.setPropertyCondition(Conditions.isNotNull()) //transfer only not null props from src-> dest
+		.setMatchingStrategy(MatchingStrategies.STRICT);//transfer the props form src -> dest which match by name & data type
+	
+		return mapper;
 	}
 
 }
